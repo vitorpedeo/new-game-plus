b1873a5550514a817013fa55acefc8d7cdd09e7b
vitorpedeo <vitorpereira@discente.ufg.br>
2020-07-13 15:22:52 -0300

    :sparkles:Create new account page

--- web/src/pages/Register/index.js
+++ web/src/pages/Register/index.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import { FaUserAlt } from 'react-icons/fa';
+
+import LogoHeader from '../../components/LogoHeader';
 
 import './styles.scss';
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className='register-container'>
+      <LogoHeader />
+      <h1>Criar conta</h1>
+      <form className='register-form'>
+        <div className='avatar-input-container'>
+          <label htmlFor=''>Imagem do Perfil</label>
+          <div className='avatar'>
+            <FaUserAlt color='#fff' size={60} className='avatar-icon' />
+          </div>
+        </div>
+        <div className='input-container'>
+          <label htmlFor=''>Nome</label>
+          <input type='text' />
+        </div>
+        <div className='input-container'>
+          <label htmlFor=''>WhatsApp</label>
+          <input type='text' />
+        </div>
+        <div className='select-input-container'>
+          <div className='input-container'>
+            <label htmlFor=''>Estado</label>
+            <select>
+              <option value='0'>UF</option>
+              <option value=''>GO</option>
+            </select>
+          </div>
+          <div className='input-container'>
+            <label htmlFor=''>Cidade</label>
+            <select>
+              <option value='0'>Cidade</option>
+              <option value=''>Senador Canedo</option>
+            </select>
+          </div>
+        </div>
+        <div className='input-container'>
+          <label htmlFor=''>Email</label>
+          <input type='email' />
+        </div>
+        <div className='input-container'>
+          <label htmlFor=''>Senha</label>
+          <input type='password' />
+        </div>
+
+        <button className='submit-btn' type='submit'>
+          Cadastrar
+        </button>
+      </form>
     </div>
   );
 };
