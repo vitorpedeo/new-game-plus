320e4aceff658bad207fc97ce1129dd8c54104d8
vitorpedeo <vitorpereira@discente.ufg.br>
2020-07-17 08:26:38 -0300

    :sparkles:Add cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
 const path = require('path');
 
 const userRoutes = require('./routes/userRoutes');
 const gameRoutes = require('./routes/gameRoutes');
 
 const responseStandard = require('./middlewares/responseStandard');
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
